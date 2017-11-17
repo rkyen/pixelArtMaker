78f0c76458ce850f1b74166feaac44166bc4ecf9
Ryan Yen <ryan.kho.yen@gmail.com>
2017-11-17 08:09:09 -0500

    its working

--- designs.js
+++ designs.js
@@ -12,33 +12,28 @@ function makeGrid() {
     inputHeight = $('#input_height').val();
     inputWidth = $('#input_width').val();
 
-    table = document.getElementById('#pixel_canvas');
+    table = document.getElementById('pixel_canvas');
 
     color = $("#colorPicker");
 
 
     // loop to create grid and add event listner to each cell
-    for (let i = 1; i < inputHeight; i++) {
-        let row = table.insertRow(i);
-        for (let j = 1; j < inputWidth; j++) {
-            let cell = row.insertCell(j);
-            cell.addEventListener('click', addEvent(cell));
+
+    for (let i = 0; i < inputHeight; i++) {
+        let row = table.insertRow();
+        for (let j = 0; j < inputWidth; j++) {
+            let cell = row.insertCell();
+            cell.addEventListener('click', function() {
+                cell.style.backgroundColor = color.val(); //Colors in the cell that was clicked and color change.
+            });
         }
-    }
-
-    //Colors in the cell that was clicked and color change.
-
-    let colorSelect = function(cell) {
-        cell.addEventListener('click', function() {
-            cell.style.backgroundColor = color.val();
-           // cell.css('background-color', color);
-        });
     }
 
 }
 
 //When button clicks, grid will be created. 
 
-$("sizePicker").submit(function(evt) {
+$("#sizePicker").submit(function(evt) {
     makeGrid();
+    evt.preventDefault();
 });
