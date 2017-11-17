da2201ce42d51755401b75d823f72408e907fce3
Ryan Yen <ryan.kho.yen@gmail.com>
2017-11-17 06:57:28 -0500

    First commit

--- designs.js
+++ designs.js
@@ -3,8 +3,42 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+
 function makeGrid() {
 
-// Your code goes here!
+    let inputHeight, inputWidth, table, color;
+
+    inputHeight = $('#input_height').val();
+    inputWidth = $('#input_width').val();
+
+    table = $('#pixel_canvas');
+
+    color = $("#colorPicker");
+
+
+    // loop to create grid and add event listner to each cell
+    for (let i = 1; i < inputHeight; i++) {
+        let row = table.insertRow(i);
+        for (let j = 1; j < inputWidth; j++) {
+            let cell = row.insertCell(j);
+            cell.addEventListener('click', addEvent(cell));
+        }
+    }
+
+    //Colors in the cell that was clicked and color change.
+
+    let colorSelect = function(cell) {
+        cell.addEventListener('click', function() {
+            cell.css('background-color', color);
+        });
+    }
 
 }
+
+//When button clicks, grid will be created. 
+
+$("input[type='submit']").on('click', function(evt) {
+    evt.preventDefault();
+    makeGrid();
+});
